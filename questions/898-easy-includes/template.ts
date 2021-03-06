31882c4dd6c16929c3e239a870a4950380d550ab
Ernest <iendeavor.github.io@gmail.com>
2021-03-06 23:32:03 +0800

    solve

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,5 @@
-type Includes<T extends readonly any[], U> = any;
+type Includes<T extends readonly any[], U> = T[0] extends never
+  ? false
+  : U extends T[0]
+    ? true
+    : Includes<T extends [any, ...infer R] ? R : never[], U>;
